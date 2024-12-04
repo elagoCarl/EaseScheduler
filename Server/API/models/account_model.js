4121295c1de96803f1edd9f7897106ac1bd484ed
elagoCarl <157703384+elagoCarl@users.noreply.github.com>
2024-12-04 17:14:04 +0800

    Fixed connection db to AIVEN

---
+++ Server/API/models/account_model.js
@@ -0,0 +1,47 @@
+module.exports = (sequelize, DataTypes) => {
+
+    const Account = sequelize.define('Account', {
+      Name: {
+          type: DataTypes.STRING,
+          allowNull: false,
+          validate: {
+              notEmpty: { msg: "Name is required." }
+          }
+      },
+      Email: {
+          type: DataTypes.STRING,
+          allowNull: false,
+          unique: true,
+          validate: {
+              isEmail: { msg: "Valid Email is required." },
+              notEmpty: { msg: "Email is required." }
+          }
+      },
+      Password: {
+          type: DataTypes.STRING,
+          allowNull: false,
+          validate: {
+              notEmpty: { msg: "Password is required." },
+              len: {
+                  args: [8],
+                  msg: "Minimum password length should be 8 characters."
+              }
+          }
+      },
+      Role: {
+          type: DataTypes.STRING
+          // If you want Role to be required, uncomment the validation below:
+          // allowNull: false,
+          // validate: {
+          //     notEmpty: { msg: "Role is required." }
+          // }
+      },
+      verified: {
+          type: DataTypes.BOOLEAN,
+          defaultValue: false
+      }
+  }, {
+      timestamps: true
+  });
+    return Account
+  }
--- Server/API/models/index.js
+++ Server/API/models/index.js
@@ -9,16 +9,26 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 require('dotenv').config()
+// const mysql = require('mysql2')
 
-const mysql = require('mysql2')
 
+// let sequelize;
+// if (config.use_env_variable) {
+//   sequelize = new Sequelize(process.env[config.use_env_variable], config);
+// } else {
+//   sequelize = new Sequelize(config.database, config.username, config.password, config);
+// }
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+const sequelize = new Sequelize(
+  process.env.DB_DATABASE,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    port: process.env.DB_PORT,
+  }
+);
 
 fs
   .readdirSync(__dirname)
@@ -43,23 +53,20 @@ Object.keys(db).forEach(modelName => {
 
 
 //ARROW FUNCTION THAT CHECKS IF THE CONNECTION IS SUCCESSFUL OR NOT.
-const connectDB = () => {
+// const connectDB = () => {
 
-  const sequelize = new Sequelize(process.env.uri)
-  sequelize.authenticate()
-    .then((result) => {
-      console.log("Successfully connected to db")
-    })
-    .catch((err) => {
-      console.log("Failed connection to db")
-    })
-}
+//   const sequelize = new Sequelize(process.env.uri)
+//   sequelize.authenticate()
+//     .then((result) => {
+//       console.log("Successfully connected to db")
+//     })
+//     .catch((err) => {
+//       console.log("Failed connection to db")
+//     })
+// }
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
-module.exports = {
-  db,
-  mysql,
-  connectDB
-};
+
+module.exports = db;
