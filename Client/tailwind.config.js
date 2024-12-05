8003c5500b8e5a606d27257b34b7d588c49fb51c
Ro Padilla <padilla2120890@mkt.ceu.edu.ph>
2024-12-05 09:38:05 +0800

    Merge from manila to development
    Merge from manila to development

--- Client/tailwind.config.js
+++ Client/tailwind.config.js
@@ -4,7 +4,29 @@ export default {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+<<<<<<< Updated upstream
     extend: {},
+=======
+    extend: {
+      screens: {
+        'xxs': '320px',  // Extra Extra Small screens
+        'xs': '480px',   // Extra Small screens
+      },
+      colors: {
+          'customBlue': '#16689B',
+          'customLightBlue': '#92B6C3',
+        },
+      spacing: {
+        ...Array.from({ length: 1000 }, (_, i) => i + 1).reduce(
+          (acc, val) => ({
+            ...acc,
+            [val]: `${val}px`, // e.g., '1': '1px', '200': '200px'
+          }),
+          {} // Initial accumulator value for reduce
+        ),
+      },
+    },
+>>>>>>> Stashed changes
   },
   plugins: [],
 };
