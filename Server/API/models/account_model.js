ee65125fbbfe989ef20169fd3d8d25ceb1b435ae
icmags <magnayeiancarl@gmail.com>
2024-12-05 21:08:55 +0800

    Resolve Conflict

--- Server/API/models/account_model.js
+++ Server/API/models/account_model.js
@@ -27,47 +27,6 @@ module.exports = (sequelize, DataTypes) => {
                 }
             }
         },
-        isAdmin: {
-            type: DataTypes.BOOLEAN,
-            defaultValue: false
-        },
-        verified: {
-            type: DataTypes.BOOLEAN,
-            defaultValue: false
-        }
-    }, {
-        timestamps: true
-    });
-    
-    return Account
-}
-        Name: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                notEmpty: { msg: "Name is required." }
-            }
-        },
-        Email: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            unique: true,
-            validate: {
-                isEmail: { msg: "Valid Email is required." },
-                notEmpty: { msg: "Email is required." }
-            }
-        },
-        Password: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                notEmpty: { msg: "Password is required." },
-                len: {
-                    args: [8],
-                    msg: "Minimum password length should be 8 characters."
-                }
-            }
-        },
         Roles: {
             type: DataTypes.STRING,
             allowNull: false,
@@ -82,6 +41,6 @@ module.exports = (sequelize, DataTypes) => {
     }, {
         timestamps: true
     });
-
-    return Account;
-};
+    
+    return Account
+}
