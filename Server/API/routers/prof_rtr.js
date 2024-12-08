7c12d0177a2849d6144f67a69617e4195c9175b1
icmags <magnayeiancarl@gmail.com>
2024-12-08 19:06:31 +0800

    Add addCourseProf, and deleteCourseProf

--- Server/API/routers/prof_rtr.js
+++ Server/API/routers/prof_rtr.js
@@ -7,5 +7,8 @@ router.get('/getAllProf', prof_ctrl.getAllProf)
 router.get('/getProf/:id', prof_ctrl.getProf)
 router.delete('/deleteProf/:id', prof_ctrl.deleteProf)
 router.put('/updateProf/:id', prof_ctrl.updateProf)
+router.post('/addCourseProf', prof_ctrl.addCourseProf)
+router.delete('/deleteCourseProf', prof_ctrl.deleteCourseProf)
+// router.get('/getProfsByCourse/:id', prof_ctrl.getProfsByCourse)
 
 module.exports = router;
