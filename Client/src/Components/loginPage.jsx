4280049051c7310072b93e59153bf0b3f86fdc44
elagoCarl <157703384+elagoCarl@users.noreply.github.com>
2024-12-09 19:31:55 +0800

    loginPage responsiveness

--- Client/src/Components/loginPage.jsx
+++ Client/src/Components/loginPage.jsx
@@ -6,20 +6,22 @@ import reject from './Img/reject.png'
 const loginPage = () => {
   const navigate = useNavigate();
   return (
-    <div className="bg-cover bg-center bg-no-repeat h-screen w-screen flex items-center justify-center px-40 items"
-      style={{ backgroundImage: `url(${image2})` }}>
-      <section className='justify-center items-center text-center'>
-        <button id='logoBtn' className="text-5xl xl:text-5xl font-bold text-blue-500 mb-20"
+    <div
+      className="bg-cover bg-no-repeat min-h-screen flex flex-col justify-between items-center overflow-y-auto"
+      style={{ backgroundImage: `url(${image2})` }}
+    >
+      <section className='justify-center items-center text-center m-auto w-11/12 sm:w-9/12 md:w-5/12 lg:w-4/12'>
+        <button id='logoBtn' className=" hover:scale-105 text-xl md:text-3xl lg:text-4xl font-bold text-blue-500 mb-50"
           onClick={() => navigate('/')}>EASE<span className="text-white">SCHEDULER</span></button>
 
-        <form id='blueBox' className='bg-black/40 p-80 items-center justify-center flex-col space-y-5 sm:p-60 w-[400px] sm:w-[450px]'>
+        <form id='blueBox' className='bg-black/40 p-15 md:p-30 items-center justify-center flex-col space-y-7 rounded-md w-full'>
           <div>
             <label for="email" class="text-start block mb-2 text-sm font-medium text-gray-100">Email</label>
-            <input type="email" name="email" id="email" class="text-gray-700 rounded-lg w-full p-3.5 bg-gray-100" placeholder="Email" required="" /> {/* for email*/}
+            <input type="email" name="email" id="email" class="text-gray-700 rounded-lg w-full p-8 md:p-15 bg-gray-100" placeholder="Email" required="" /> {/* for email*/}
           </div>
           <div>
             <label for="email" class="text-start block mb-2 text-sm font-medium text-gray-100">Password</label>
-            <input type="password" name="passw" id="password" class="text-gray-700 rounded-lg w-full p-3.5 bg-gray-100" placeholder="*********" required="" /> {/* for password*/}
+            <input type="password" name="passw" id="password" class="text-gray-700 rounded-lg w-full p-8 md:p-15 bg-gray-100" placeholder="*********" required="" /> {/* for password*/}
 
             <svg class="shrink-0 size-0.5" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" />
             <path class="hs-password-active:hidden" ></path>
