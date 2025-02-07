9259d90c13c6570eafa64acfdc3dd945f126031a
icmags <magnayeiancarl@gmail.com>
2025-02-07 23:28:39 +0800

    Modify profAvail, status, room controllers validation checks

--- Server/utils.js
+++ Server/utils.js
@@ -18,6 +18,13 @@ const checkMandatoryFields = (arrs)=>{
   return result
 }
 
+const validDays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+
+// CHECKS IF THE GIVEN DAY IS VALID (CASE-INSENSITIVE)
+const checkValidDay = (day) => {
+  if (typeof day !== "string") return false; // Ensure input is a string
+  return validDays.includes(day.charAt(0).toUpperCase() + day.slice(1).toLowerCase());
+}
 
 const emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
 const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#])[A-Za-z\d@$!%*?&]{8,}$/;
@@ -34,5 +41,6 @@ module.exports = {
   checkIfNull, 
   checkMandatoryFields,
   validateEmail,
-  validatePassword
+  validatePassword,
+  checkValidDay
 }
