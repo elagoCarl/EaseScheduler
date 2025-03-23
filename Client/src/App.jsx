624bdfb59149bbbef50b7fd621794b3f20774d37
elagoCarl <157703384+elagoCarl@users.noreply.github.com>
2025-03-23 10:25:26 +0800

    deptProg page(ro paki gawan ng prog)

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -20,6 +20,7 @@ import AccountList from "./Components/accountList";
 import AssignationsCourseProf from "./Components/assignationsCourseProf";
 import Page404 from "./Components/page404";
 import Page403 from "./Components/page403";
+import DeptProg from "./Components/deptProg";
 
 
 const App = () => {
@@ -47,6 +48,7 @@ const App = () => {
         <Route path="/sectionTimetable" element={<SectionTimetable />} />
         <Route path="/accountList" element={<AccountList />} />
         <Route path="/assignationsCourseProf" element={<AssignationsCourseProf />} />
+        <Route path="/deptProg" element={<DeptProg />} />
 
         {/**ERROR handling routes */}
         <Route path="*" element={<Page404/>} />
