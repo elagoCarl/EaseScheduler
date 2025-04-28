30f9909ebba41b7684836491cd664b02fca7a3f0
Ro Padilla <157687882+rorupabi@users.noreply.github.com>
2025-04-29 00:24:23 +0800

    ui fix

--- Client/src/Components/addConfigSchedule.jsx
+++ Client/src/Components/addConfigSchedule.jsx
@@ -147,7 +147,7 @@ const renderManualSchedulingSection = () => {
             className="text-blue-600 hover:text-blue-800 text-xs flex items-center"
             disabled={activeMode !== 'manual'}
           >
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
             </svg>
             Check {formData.professorName}&apos;s Availability
@@ -1137,7 +1137,7 @@ const renderManualSchedulingSection = () => {
                       onClick={() => handleCheckAvailability(formData.professorId)}
                       className="text-blue-600 hover:text-blue-800 text-xs flex items-center"
                     >
-                      <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                      <svg xmlns="http://www.w3.org/2000/svg" className="h-16 w-16 mr-4 ml-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
                       </svg>
                       Check {formData.professorName}&apos;s Availability
