6503b21c1c9cc3defcc42f1da7996909660bf16b
icmags <magnayeiancarl@gmail.com>
2025-05-10 17:51:50 +0800

    Debug adding Assignation Integration

--- Server/API/routers/assignation_rtr.js
+++ Server/API/routers/assignation_rtr.js
@@ -8,7 +8,7 @@ router.post('/addAssignation', assignation_ctrl.addAssignation);
 router.get('/getAllAssignations', assignation_ctrl.getAllAssignations);
 router.get('/getAssignation/:id', assignation_ctrl.getAssignation);
 router.get('/getAllAssignationsByDept/:id', assignation_ctrl.getAllAssignationsByDept);
-router.get('/getAllAssignationsByDeptInclude/:id', assignation_ctrl.getAllAssignationsByDeptInclude);
+router.post('/getAllAssignationsByDeptInclude', assignation_ctrl.getAllAssignationsByDeptInclude);
 router.put('/updateAssignation/:id', assignation_ctrl.updateAssignation);
 router.delete('/deleteAssignation/:id', assignation_ctrl.deleteAssignation);
 
